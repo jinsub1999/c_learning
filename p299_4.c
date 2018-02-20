fd53b34babbc817fd4b6d2ef6896b3f896641f53
Hong Jin-Seop <36567398+jinsub1999@users.noreply.github.com>
2018-02-20 11:05:49 +0900

    포인터를 이용한 배열 요소의 출력과 연산
    페이지 299, 연습문제 4. (간략화 한 버전)

--- p299_4.c
+++ p299_4.c
@@ -5,26 +5,18 @@ int main(void)
 	double ary[5] = {1.2 , 3.5 , 7.4 , 0.5 , 10.0 };
 	double *pa = ary;
 	double *pb = ary + 2;
-
+	double total = 0;
 	int i;
 	
 	printf("배열 요소의 값 : ");
 	for (i = 0; i < (sizeof(ary) / sizeof(ary[0])); i++)
 	{
 		printf("%.1lf   ", *pa);
-		pa++;
-	}
-	pa = ary;
-	printf("\n평균 : ");
-
-	double total = 0;
-	for (i = 0; i < (sizeof(ary) / sizeof(ary[0])); i++)
-	{
 		total += *pa;
-
 		pa++;
 	}
 	double average = total / (sizeof(ary) / sizeof(ary[0]));
-
-	printf("%.2lf\n", average);
+	
+	
+	printf("\n평균 : %.2lf\n", average);
 }

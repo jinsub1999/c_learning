14a6304f71e3f4f21e6265903545b57257fe0640
Hong Jin-Seop <36567398+jinsub1999@users.noreply.github.com>
2018-04-08 13:52:41 +0900

    p.376_4

--- p376_4.c
+++ p376_4.c
@@ -4,10 +4,13 @@
 int main(void)
 {
 	char str1[80], str2[80];
+	printf("input two words : ");
 	scanf("%s %s", &str1, &str2);
-
+	
+	printf("longer word : ");
 	if (strlen(str1) >= strlen(str2)) printf("%s\n", str1);
 	else if (strlen(str1) < strlen(str2)) printf("%s\n", str2);
+	printf("earlier word : ");
 	if (strcmp(str1, str2) >= 0) printf("%s\n", str2);
 	else if (strcmp(str1, str2) == -1) printf("%s\n", str1);
 }
